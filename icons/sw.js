43fa74b4feabef674fb5466dd5cf7a522361bd62
Polina264 <159820807+Polina264@users.noreply.github.com>
2024-07-25 12:17:16 +0300

    Update and rename sw.js to icons/sw.js

---
+++ icons/sw.js
@@ -0,0 +1,26 @@
+const staticCacheName = 'CacheSpace1';
+
+const assetUrls = [
+  '/icons',
+  '/style.css',
+  '/background',
+  '/index.html',
+]
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCacheName)
+    await cache.addAll(assetUrls)
+})
+self.addEventListener('activate', event => {
+  console.log('[SW]: activate');
+})
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch', event.request.url);
+  event.respondWith(cacheFirst(event.request));
+})
+
+async function cacheFirst(request) {
+  const cached = await cache.match(request);
+  return cached ?? await fetch(request);
+}
